6a549d180f8a3e4147ceae00a8df828ecaaaa97e
bebeorca <dimasrdyo@gmail.com>
2024-06-29 18:10:02 +0700

    frist

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <RouterLink/>
+  </div>
+  <div class=" h-screen flex justify-center items-center">
+    <div class="w-40 h-40 bg-blue-950 rounded-md shadow-lg flex justify-center items-center text-white">
+      <h1 class="font-bold text-2xl">Halo</h1>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router';
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
